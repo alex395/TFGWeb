46671006ed56ef6bec5c94dd5d684ea706c5be25
alex395 <ardoyalex@gmail.com>
2018-09-27 13:33:07 +0200

    MVC

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Noticia(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField(default=timezone.now)
+    tipoNotificacion = models.TextField()
+
+def publish(self):
+    self.fecha = timezone.now()
+    self.save()
+
+def __str__(self):
+    return self.title
--- web/views.py
+++ web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Noticia
+from django.utils import timezone
 # Create your views here.
+def index(request):
+        return render(request, 'index/index.html', {})
+
+def noticiaList(request):
+        noticias = Noticia.objects.filter(fecha__lte=timezone.now()).order_by('fecha')
+        return render(request, 'noticia/list.html', {'noticias': noticias})
