0cc8460bd3330d4fe00df39c3f274027f2879796
alex395 <ardoyalex@gmail.com>
2018-11-30 23:27:10 +0000

    Last commit

--- seguridad/forms.py
+++ seguridad/forms.py
@@ -5,5 +5,5 @@ from django.forms.widgets import PasswordInput, TextInput
 
 #Formulario de login.
 class CustomAuthForm(AuthenticationForm):
-    username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Email'}))
+    username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Usuario'}))
     password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Contraseña'}))
